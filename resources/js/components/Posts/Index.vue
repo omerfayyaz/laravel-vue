a997c3c7b9a2ad04b1a4502f1431f0736cae04e7
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-09-29 22:27:55 +0500

    Adding :key to post v-for

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -19,7 +19,7 @@
                     </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                    <tr v-for="post in posts">
+                    <tr v-for="post in posts" :key="post.id">
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.id }}</td>
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.title }}</td>
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.content}}</td>
