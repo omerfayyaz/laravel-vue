ffd6f4d7141a32b4b1667be9ad7ab3877d7d7dfb
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-09-30 22:37:17 +0500

    add an async method for getting the posts.

--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -2,4 +2,11 @@ import { ref } from "vue";
 
 export default function usePosts() {
     const posts = ref([])
+
+    const getPosts =  async () => {
+        axios.get('/api/posts')
+        .then(response => {
+            posts.value = response.data;
+        })
+    }
 }
