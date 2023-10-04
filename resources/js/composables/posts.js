59386bf2591426cc17914ba1d8aed5a82edd0458
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-10-04 23:30:59 +0500

    posts[] to posts{}

--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 
 export default function usePosts() {
-    const posts = ref([])
+    const posts = ref({})
 
     const getPosts =  async () => {
         axios.get('/api/posts')
