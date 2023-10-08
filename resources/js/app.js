5b9730b1d21b322f132c165890c8e44b75f4a1e9
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-10-08 17:08:29 +0500

    Vue Component for Second Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,11 @@ import '@/bootstrap';
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import PostsIndex from '@/components/Posts/Index.vue';
+import PostsCreate from '@/components/Posts/Create.vue';
 
 const routes = [
     { path: '/', component: PostsIndex },
+    { path: '/posts/create', component: PostsCreate}
 ]
 
 const router = createRouter({
