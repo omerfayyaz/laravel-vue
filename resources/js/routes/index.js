955494bb5b661cd660800e66faa867bff09d28a7
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-10-15 19:47:10 +0500

    Routes in groups

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,29 +1,51 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import AuthenticatedLayout from '@/layouts/Authenticated.vue';
+import GuestLayout from '@/layouts/Guest.vue';
+
 import PostsIndex from '@/components/Posts/Index.vue';
 import PostsCreate from '@/components/Posts/Create.vue';
 import PostsEdit from '@/components/Posts/Edit.vue';
+import Login from '@/components/Auth/Login.vue';
 
 
 const routes = [
     {
-        path: '/',
-        name: 'posts.index',
-        component: PostsIndex,
-        meta: {title: 'Posts'}
+        component: GuestLayout,
+        children: [
+            {
+                path: '/login',
+                name: 'login',
+                component: Login,
+            },
+        ]
+
     },
     {
-        path: '/posts/create',
-        name: 'posts.create',
-        component: PostsCreate,
-        meta: {title: 'Add New Post'}
+        component: AuthenticatedLayout,
+        children: [
+            {
+                path: '/posts',
+                name: 'posts.index',
+                component: PostsIndex,
+                meta: {title: 'Posts'}
+            },
+            {
+                path: '/posts/create',
+                name: 'posts.create',
+                component: PostsCreate,
+                meta: {title: 'Add New Post'}
+            },
+            {
+                path: '/posts/edit/:id',
+                name: 'posts.edit',
+                component: PostsEdit,
+                meta: {title: 'Edit Post'}
+            },
+        ]
+
     },
-    {
-        path: '/posts/edit/:id',
-        name: 'posts.edit',
-        component: PostsEdit,
-        meta: {title: 'Edit Post'}
-    },
+
 ]
 
 export default createRouter({
