230b4dd25e6251f57a9ac553a15df3a912477be2
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-10-17 20:46:49 +0500

    Login button fix

--- resources/js/components/Auth/Login.vue
+++ resources/js/components/Auth/Login.vue
@@ -38,7 +38,7 @@
 
         <!-- Buttons -->
         <div class="flex items-center justify-end mt-4">
-            <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150 ml-4" :class="{ 'opacity-25': processing }" :disabled="processing"></button>
+            <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150 ml-4" :class="{ 'opacity-25': processing }" :disabled="processing">Login</button>
         </div>
     </form>
 </template>
